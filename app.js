c4806a22ca38bc2d94280d2fd8d3d3cef07528c7
IjazDriscollGL <ijaz.driscoll@geniuslead.com>
2021-11-24 15:33:27 +0000

    Task 1

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 console.log("It's Kanye time...");
+let datasetInh1 = document.querySelector("#kanye-quote");
+//write an async function called getQuote
+async function getQuote(event) {
+const response = await fetch('https://api.kanye.rest/');
+const data = await response.json();
+datasetInh1.innerHTML = `<h1>${data.quote}</h1>`;
+}
+getQuote();
+//Use fetch to send a GET request to https://api.kanye.rest/ and store the resolved value of the returned promise in a variable called response.
